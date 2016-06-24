2680419dccc6751bcf0340872416b74b06fc0d72
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-06-24 17:05:31 +0300

    Hackerrank ai maze escape test1 won.

--- hackerrank/ai-maze-escape.cpp
+++ hackerrank/ai-maze-escape.cpp
@@ -9,16 +9,22 @@ int wall = 35;
 int empty = 45;
 int door = 101;
 
-void nextMove(vector<string> grid) {
-    int upLeft = grid[0][0];
-    int up = grid[0][1];
-    int upRight = grid[0][2];
-    int left = grid[1][0];
-    int right = grid[1][2];
-    int downLeft = grid[2][0];
-    int down = grid[2][1];
-    int downRight = grid[2][2];
-
+void nextMove() {
+    string s;
+    cin >> s;
+//    cout << s << endl;
+    int upLeft = s[0];
+    int up = s[1];
+    int upRight = s[2];
+    cin >> s;
+//    cout << s << endl;
+    int left = s[0];
+    int right = s[2];
+    cin >> s;
+//    cout << s << endl;
+    int downLeft = s[0];
+    int down = s[1];
+    int downRight = s[2];
 
     if (up == door || (up == empty && right == wall)) {
         cout << "UP";
@@ -35,14 +41,9 @@ int main() {
     int id;
     cin >> id;
 
-    vector<string> grid;
-    for(int i=0; i<3; i++) {
-        string s; cin >> s;
-        cout << s << endl;
-        grid.push_back(s);
-    }
-
-    nextMove(grid);
+//    for (int i=0; i<4; i++) {
+        nextMove();
+//    }
 
     return 0;
 }
