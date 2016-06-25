85c3c4935f280ea33b30410c5dc7d0c9316f5f52
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-06-25 14:11:00 +0300

    Euler problem 30 solved.

--- project-euler/problem30.cpp
+++ project-euler/problem30.cpp
@@ -24,16 +24,17 @@ public:
     void operator--();
     void operator--(int);
     friend ostream& operator<<(ostream& os, const Num& n);
+    unsigned long long operator[](int i);
+    int length();
 };
 
 void Num::operator=(unsigned long long num) {
     while (num > 0) {
-        cout << num << endl;
-
+//        cout << num << endl;
         this->numVector.push_back(num % 10);
         num = num / 10;
     }
-    cout << num << endl;
+//    cout << num << endl;
 
 }
 void Num::operator++() {
@@ -53,12 +54,17 @@ void Num::operator--() {
 void Num::operator--(int) {
     --*this;
 }
-ostream& operator<<(ostream& os, const Num& n)
-{
+ostream& operator<<(ostream& os, const Num& n) {
     for (int i = n.numVector.size() - 1; i >= 0; i--) {
         os << n.numVector[i];
     }
     return os;
+}
+unsigned long long Num::operator[](int i) {
+    return this->numVector[i];
+}
+int Num::length() {
+    return this->numVector.size();
 }
 
 int main() {
@@ -73,22 +79,45 @@ int main() {
     pows.push_back(pow(8,5)); // 32768
     pows.push_back(pow(9,5)); // 59049
 
+//    pows.push_back(pow(2,4)); // 32
+//    pows.push_back(pow(3,4)); // 243
+//    pows.push_back(pow(4,4)); // 1024
+//    pows.push_back(pow(5,4)); // 3124
+//    pows.push_back(pow(6,4)); // 7776
+//    pows.push_back(pow(7,4)); // 16807
+//    pows.push_back(pow(8,4)); // 32768
+//    pows.push_back(pow(9,4)); // 49049
+
     long max = 354294;
+//    long max = 9475;
 
     Num num;
     num = max;
 
     cout << num << endl;
 
-    num--;
-    num--;
-    num--;
-    num--;
-    num--;
+//    vector<long> sums;
+    long sumx = 0;
+    for (int n = max; n > 9; n--) {
+        int sum = 0;
+        int i;
+        for (i = 0; i < num.length(); i++) {
+            sum += pows[num[i]];
+            if (sum > n) {
+                break;
+            }
+        }
+        if (i == num.length() && sum == n) {
+            cout << "Found sum: " << sum << endl;
+            sumx += sum;
+//            sums.push_back(sum);
+        }
 
-    cout << num;
+        num--;
+    }
 
+    cout << num << endl;
+    cout << sumx << endl;
 
-//    for (int n = max; n > )
-
+    // Correct answer: 443839
 }
