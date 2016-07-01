3ddb0e9098f13ea0b1d483ef7c8572d002e7c624
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-07-01 20:01:37 +0300

    Euler 303.

--- project-euler/problem303.cpp
+++ project-euler/problem303.cpp
@@ -1,86 +1,38 @@
 #include <iostream>
-#include "Num.h"
+#include <cmath>
 
 using namespace std;
 
-//int multiplicationTable[9][9];
+int main() {
+    int startNum = 10;
+    for (int x = 3; x < 10; x++) {
+        int num = startNum;
+//        cout << "f(" << x << ")=" << endl;
+        cout << "f(" << x << ")=";
+        bool found = false;
+        for (int i = 2; !found && i < 10; i++) {
+            int diipa = num;
+            while (true) {
+                for (int j = 1; j < 3; j++) {
+                    for (int k = 0; k < 3; k++) {
+                        //                cout << "Try: " << num << endl;
 
-bool digitsLessThanThree(int i) {
-    if (i % 10 > 2) {
-        return false;
-    }
-    int m = 1;
-    int b = 1;
-    while (b != 0) {
-        int d = m * 10;
-//        int a = i % d;
-        int b = i / d;
-        m++;
-    }
-}
-
-int magic(Num num) {
-    bool found = false;
-    for (int i = 12598; i < 12599; i++) {
-//        cout << i << endl;
-//        int result;
-//        int j = 0;
-//        while (result < 3) {
-        int b = 0;
-        for (int j = 0; j < num.length(); j++) {
-            cout << "----" << endl;
-            cout << i << ' ' << num[j] << ' ' << b << endl;
-
-
-            int c = i * num[j] + b;
-
-            cout << c << endl;
-            cout << "----" << endl;
-
-            int d = 10;
-//            int d = (j + 1) * 10;
-            int a = c % d;
-            b = c / d;
-            cout << a << endl;
-            cout << b << endl;
-            if (j < num.length() - 1) {
-//                cout << (i * num[j + 1]);
-//                if (a > 2 || ((i * num[j + 1] % 10) + b) > 2) {
-                if (a > 2) {
-                    break;
-                }
-            } else {
-//                cout << a << endl;
-//                cout << b << endl;
-                if (a < 3 && b < 3) {
-                    cout << i * num.getValue();
-                    found = true;
-                    break;
+                        if (num % x == 0) {
+                            //                    cout << "Found: " << num << endl;
+                            cout << num << endl;
+                            found = true;
+                            break;
+                        }
+                        num++;
+                    }
+                    num += 10 * j;
                 }
             }
+            num = pow(10, i);
         }
-        if (found) {
-            break;
+        if (x % 10 == 0) {
+            startNum = x;
         }
     }
 
-    return 0;
-}
-
-int main() {
-    Num num;
-    num = 89;
-    magic(num);
-
-    // Populate multiplication table
-//    for (int i = 1; i < 10; i++) {
-//        for (int j = 1; j < 10; j++) {
-//            multiplicationTable[i][j] = i * j;
-//        }
-//    }
-
-//    while (num > 8) {
-//        magic(num);
-//        num--;
-//    }
 }
