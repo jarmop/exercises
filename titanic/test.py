fc00367feddef66d351fa669b55d14c94edb2dd7
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-08-14 18:22:58 +0300

    Dataquest Kaggle tutorial 2 completed.

--- titanic/test.py
+++ titanic/test.py
@@ -9,6 +9,7 @@ import re
 import operator
 from sklearn.feature_selection import SelectKBest, f_classif
 import matplotlib.pyplot as plt
+from sklearn.ensemble import GradientBoostingClassifier
 
 # A dictionary mapping family name to id
 family_id_mapping = {}
@@ -77,6 +78,23 @@ def add_title(titanic):
     # Add in the title column.
     titanic["Title"] = titles
 
+def add_title_test(titanic):
+    # Get all the titles and print how often each one occurs.
+    titles = titanic["Name"].apply(get_title)
+    # print(pandas.value_counts(titles))
+
+    # Map each title to an integer.  Some titles are very rare, and are compressed into the same codes as other titles.
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8,
+                     "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2, "Dona": 10}
+    for k, v in title_mapping.items():
+        titles[titles == k] = v
+
+    # Verify that we converted everything.
+    # print(pandas.value_counts(titles))
+
+    # Add in the title column.
+    titanic["Title"] = titles
+
 # A function to get the id given a row
 def get_family_id(row):
     # Find the last name by splitting on a comma
@@ -93,7 +111,7 @@ def get_family_id(row):
         family_id_mapping[family_id] = current_id
     return family_id_mapping[family_id]
 
-def add_family_size(titanic):
+def add_family_id(titanic):
     # Get the family ids with the apply method
     family_ids = titanic.apply(get_family_id, axis=1)
 
@@ -114,7 +132,18 @@ def add_features(titanic):
 
     add_title(titanic)
 
-    add_family_size(titanic)
+    add_family_id(titanic)
+
+def add_features_test(titanic):
+    # Generating a familysize column
+    titanic["FamilySize"] = titanic["SibSp"] + titanic["Parch"]
+
+    # The .apply method generates a new series
+    titanic["NameLength"] = titanic["Name"].apply(lambda x: len(x))
+
+    add_title_test(titanic)
+
+    add_family_id(titanic)
 
 def show_best_predictors(titanic):
     predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]
@@ -208,18 +237,96 @@ def random_forest():
     # Take the mean of the scores (because we have one for each fold)
     print(scores.mean())
 
+def ensembling():
+    # The algorithms we want to ensemble.
+    # We're using the more linear predictors for the logistic regression, and everything with the gradient boosting classifier.
+    algorithms = [
+        [GradientBoostingClassifier(random_state=1, n_estimators=25, max_depth=3),
+         ["Pclass", "Sex", "Age", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]],
+        [LogisticRegression(random_state=1),
+         ["Pclass", "Sex", "Fare", "FamilySize", "Title", "Age", "Embarked"]]
+    ]
+
+    # Initialize the cross validation folds
+    kf = KFold(titanic.shape[0], n_folds=3, random_state=1)
+
+    predictions = []
+    for train, test in kf:
+        train_target = titanic["Survived"].iloc[train]
+        full_test_predictions = []
+        # Make predictions for each algorithm on each fold
+        for alg, predictors in algorithms:
+            # Fit the algorithm on the training data.
+            alg.fit(titanic[predictors].iloc[train, :], train_target)
+            # Select and predict on the test fold.
+            # The .astype(float) is necessary to convert the dataframe to all floats and avoid an sklearn error.
+            test_predictions = alg.predict_proba(titanic[predictors].iloc[test, :].astype(float))[:, 1]
+            full_test_predictions.append(test_predictions)
+        # Use a simple ensembling scheme -- just average the predictions to get the final classification.
+        test_predictions = (full_test_predictions[0] + full_test_predictions[1]) / 2
+        # Any value over .5 is assumed to be a 1 prediction, and below .5 is a 0 prediction.
+        test_predictions[test_predictions <= .5] = 0
+        test_predictions[test_predictions > .5] = 1
+        predictions.append(test_predictions)
+
+    # Put all the predictions together into one array.
+    predictions = np.concatenate(predictions, axis=0)
+
+    # Compute accuracy by comparing to the training data.
+    accuracy = sum(predictions[predictions == titanic["Survived"]]) / len(predictions)
+    print(accuracy)
+
+def create_submission():
+    predictors = ["Pclass", "Sex", "Age", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]
+
+    algorithms = [
+        [GradientBoostingClassifier(random_state=1, n_estimators=25, max_depth=3), predictors],
+        [LogisticRegression(random_state=1), ["Pclass", "Sex", "Fare", "FamilySize", "Title", "Age", "Embarked"]]
+    ]
+
+    full_predictions = []
+    for alg, predictors in algorithms:
+        # Fit the algorithm using the full training data.
+        alg.fit(titanic[predictors], titanic["Survived"])
+        # Predict using the test dataset.  We have to convert all the columns to floats to avoid an error.
+        predictions = alg.predict_proba(titanic_test[predictors].astype(float))[:, 1]
+        full_predictions.append(predictions)
+
+    # The gradient boosting classifier generates better predictions, so we weight it higher.
+    predictions = (full_predictions[0] * 3 + full_predictions[1]) / 4
+
+    # print(predictions)
+
+    predictions[predictions <= .5] = 0
+    predictions[predictions > .5] = 1
+    predictions = predictions.astype(int)
+
+    # print(predictions)
+
+    submission = pandas.DataFrame({
+        "PassengerId": titanic_test["PassengerId"],
+        "Survived": predictions
+    })
+
+    submission.to_csv("kaggle.csv", index=False)
+
 # We can use the pandas library in python to read in the csv file.
 # This creates a pandas dataframe and assigns it to the titanic variable.
 titanic = pandas.read_csv("train.csv")
-# titanic_test = pandas.read_csv("test.csv")
+titanic_test = pandas.read_csv("test.csv")
 
 clean_up(titanic)
 add_features(titanic)
-# clean_up_test(titanic_test)
+clean_up_test(titanic_test)
+add_features_test(titanic_test)
+
 # show_best_predictors(titanic)
 # linear_regression()
 # logistic_regression()
-random_forest()
+# random_forest()
+# ensembling()
+
+create_submission()
 
 # predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 #
