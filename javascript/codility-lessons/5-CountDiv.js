3552a61ce4e71eee72a92ceaf978c5c6fc4dd778
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-04-23 22:57:19 +0300

    Add codility lessons.

---
+++ javascript/codility-lessons/5-CountDiv.js
@@ -0,0 +1,20 @@
+function solution(A, B, K) {
+  if (K > B && B !== 0) {
+    return 0
+  }
+  if (A === B) {
+    return A % K === 0 ? 1 : 0;
+  }
+  let i = A === 0 || K <= A ? A + A % K : K;
+  let j = B - B % K;
+
+  return ((j - i) / K) + 1;
+}
+
+let tests = [
+  [11,14,2]
+];
+
+for (test of tests) {
+  console.log(solution(...test));
+}
